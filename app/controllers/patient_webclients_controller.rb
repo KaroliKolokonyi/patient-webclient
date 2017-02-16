570efea085357ffc7fa71e851af53493228a7125
Karoli Kolokonyi <karoli@kepler.org>
2017-02-16 14:28:22 +0200

    API comminication

--- app/controllers/patient_webclients_controller.rb
+++ app/controllers/patient_webclients_controller.rb
@@ -2,15 +2,15 @@ class PatientWebclientsController < ApplicationController
 
 
 def show 
-  @patient = Unirest.get("http:http://localhost:3000/api/v2/patients/#{params[:id]}.json").body
+  @patient = Unirest.get("http://localhost:3000/api/v2/patients/#{params[:id]}.json").body
 end 
 
 def index
-  @patients = Unirest.get("http:http://localhost:3000/api/v2/patients.json").body
+  @patients = Unirest.get("http://localhost:3000/api/v2/patients.json").body
 end
 
 def create
-  @patient = Unirest.post("http:http://localhost:3000/api/v2/patients.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate]}).body
+  @patient = Unirest.post("http://localhost:3000/api/v2/patients.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate]}).body
 
   redirect_to "/patient/#{@patient['id']}"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  post "/patients", to: 'patient_webclients#create'
+ 
+  get "/patients", to: 'patient_webclients#index'
+  get "/patients/new", to: 'patient_webclients#new'
+  get "/patients/:id", to: 'patient_webclients#show'
+
 end
